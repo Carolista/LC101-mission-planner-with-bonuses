89b1861074e9b926f4d46f5f4c6299f39ab7d898
Carolista <cjones.renaissancewoman@gmail.com>
2021-07-07 23:41:27 -0500

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CrewComponent } from './crew/crew.component';
+import { EquipmentComponent } from './equipment/equipment.component';
+import { ExperimentsComponent } from './experiments/experiments.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrewComponent,
+    EquipmentComponent,
+    ExperimentsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
